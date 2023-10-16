23b0e9ccec910e3ba0ca642f1892119da9ce84fb
karine24 <karine.m.tavares@hotmail.com>
2023-10-15 22:11:02 -0300

    first commit
    Beginning of home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { StoresComponent } from './pages/stores/stores.component';
+import { PricesComponent } from './pages/prices/prices.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'company', component: CompanyComponent},
+  {path: 'stores', component: StoresComponent},
+  {path: 'prices', component: PricesComponent},
+  {path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { StoresComponent } from './pages/stores/stores.component';
+import { PricesComponent } from './pages/prices/prices.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    CompanyComponent,
+    StoresComponent,
+    PricesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
